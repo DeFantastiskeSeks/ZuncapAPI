8d33a0c3f67c60aa1a95bc69ea828b70d3d91fce
Rex77pro <smaurieka@gmail.com>
2023-05-08 12:42:51 +0200

    Controller + start af UnitTest

--- ZuncapAPI/Controllers/UsersController.cs
+++ ZuncapAPI/Controllers/UsersController.cs
@@ -1,22 +1,50 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ZuncapAPI.Models;
 using ZuncapAPI.Repository;
 
 namespace ZuncapAPI.Controllers
 {
     public class UsersController : Controller
     {
-        public UserRepository _repo  { get; set; }
-        public UsersController(UserRepository repo) 
-        
-        { 
-         _repo = repo;
+        public UserRepository _repo { get; set; }
+        public UsersController(UserRepository repo)
+
+        {
+            _repo = repo;
         }
 
-        [HttpPost]
-        [ProducesErrorResponseType(StatusCodes.Status201Created)]
-        [ProducesErrorResponseType(StatusCodes.Status404NotFound)]
-        //[ProducesErrorResponseType(StatusCodes.Status404NotFound)]
+        [HttpPost("add")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<User> POST([FromBody] User User)
+        {
+
+            try {
+
+                User NewUser = _repo.Create(User);
+
+                return Created($"api/User/add/{NewUser.UserId}", NewUser);
+
+
+
+            } catch(ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+                
+
+            } catch(ArgumentOutOfRangeException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex) 
+            {
+
+                return BadRequest(ex.Message);
+            }
+            
+          
+        }
 
 
     }
